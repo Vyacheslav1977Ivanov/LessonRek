379d943fe0fbb632ea51da83b60c110f14a7c233
Vyacheslav Ivanov <vya1977@mail.ru>
2023-12-01 19:49:13 +0300

    Task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿// Задача 3 : Задайте произвольный массив. Выведете его элементы,
+// начиная с конца. Использовать рекурсию, не использовать циклы
+
+
+
+int[] array = { 3,12,32,4,0,17 };
+
+int num = array.Length - 1;
+
+int ArrayNumsRevers(int num,int[] array)
+{
+    if (num < 0)
+    {
+        
+        return 0;
+    }
+    Console.Write($"{array[num]},");
+    return  ArrayNumsRevers(num - 1 , array);
+}
+Console.WriteLine($"Исходный массив: [{array[0]},{array[1]},{array[2]},{array[3]},{array[4]},{array[5]}]");
+Console.Write("Перевернутый массив: [");
+ArrayNumsRevers(num, array);
+Console.Write("]");
